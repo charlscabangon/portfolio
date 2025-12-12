d3da2b4343844bc31d83660d21a5cc8ae6a8b8ca
Charls Cabangon <charlsmercabangon@gmail.com>
2025-12-12 15:27:29 +0800

    feat: implement analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import { Analytics } from '@vercel/analytics/react';
 import { AppLayout, Header } from '@/layout';
 import { ThemeProvider } from '@/features/Theme';
 import HomePage from './pages/HomePage';
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <Header />
-      <AppLayout>
-        <HomePage />
-      </AppLayout>
-    </ThemeProvider>
+    <>
+      <ThemeProvider>
+        <Header />
+        <AppLayout>
+          <HomePage />
+        </AppLayout>
+      </ThemeProvider>
+      <Analytics />
+    </>
   );
 }
